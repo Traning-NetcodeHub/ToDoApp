da8bb7aa7a5293eaa1ba9b9bbd5ae2bedbd30aaa
Frederick Asante <Rastinga1995@hotmail.com>
2023-12-31 07:25:29 +0000

    Add project files.

---
+++ ToDoApp/Model/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApp.Model
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool isDone { get; set; } = false;
+        public DateTime Date { get; set; } = DateTime.Now;
+        public DateTime? CompletionDate { get; set; } 
+    }
+}
